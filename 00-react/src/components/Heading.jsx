5a3bf14f06733bb4f7c81b2218dc8cccd07bd649
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-05 19:21:56 +0530

    Refactor Heading component to apply container styles for improved layout

--- 00-react/src/components/Heading.jsx
+++ 00-react/src/components/Heading.jsx
@@ -1,6 +1,17 @@
 export default function Heading({ title, desc }) {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.5rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '1rem',
+    borderRadius: '6px',
+    border: '1px solid grey',
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <h1>{title}</h1>
       <h4>{desc}</h4>
     </div>
