d6932074961af44d918dae2049dfeca9116a9747
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-05 18:29:36 +0530

    day-1 (react-refresher) code

---
+++ 00-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Heading from './Heading';
+
+export default function App() {
+  return (
+    <>
+      <Heading
+        title="Welcome"
+        desc="This is welcome message"
+      />
+      <Heading
+        title="Hello World"
+        desc="This is hello world message"
+      />
+    </>
+  );
+}
---
+++ 00-react/src/Heading.jsx
@@ -0,0 +1,8 @@
+export default function Heading({ title, desc }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <h4>{desc}</h4>
+    </>
+  );
+}
