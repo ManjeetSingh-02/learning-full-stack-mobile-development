6bc92eaafaf04c1eeb44d5c1de97f8e85f82cc35
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-09 13:56:26 +0530

    Add KeyboardAvoidView component for login functionality

--- 01 - expo_basic/src/app/index.tsx
+++ 01 - expo_basic/src/app/index.tsx
@@ -1,11 +1,13 @@
 import ViewWithOtherComponents from './ViewWithOtherComponents';
 import FlatlistScreen from './Flatlist';
 import ScrollViewScreen from './ScrollView';
+import KeyboardAvoidView from './KeyboardAvoidView';
 
 export default function () {
   return (
     // <ViewWithOtherComponents />
     // <ScrollViewScreen />
-    <FlatlistScreen />
+    // <FlatlistScreen />
+    <KeyboardAvoidView />
   );
 }
