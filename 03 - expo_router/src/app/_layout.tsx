7a9357d5af89d6ec5daa8356d214b8f28fdee511
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-19 23:40:05 +0530

    Refactor RootLayout to implement protected routes based on authentication status

--- 03 - expo_router/src/app/_layout.tsx
+++ 03 - expo_router/src/app/_layout.tsx
@@ -1,12 +1,31 @@
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import { Text, View } from 'react-native';
 
+// export default function RootLayout() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 10 }}>
+//       <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Header</Text>
+//       <Slot />
+//       <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Footer</Text>
+//     </View>
+//   );
+// }
+
 export default function RootLayout() {
+  const isLoggedIn = false;
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 10 }}>
-      <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Header</Text>
-      <Slot />
-      <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Footer</Text>
-    </View>
+    <Stack>
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen
+          name='(auth)'
+          options={{ headerShown: false }}
+        />
+      </Stack.Protected>
+
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name='index' />
+      </Stack.Protected>
+    </Stack>
   );
 }
