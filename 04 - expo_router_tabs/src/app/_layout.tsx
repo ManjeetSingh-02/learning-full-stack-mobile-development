db8af6ba9bc7663a16019afb8e7328e52b8da477
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-20 00:42:28 +0530

    Comment out tab navigation and ensure drawer navigation is set up in RootLayout

--- 04 - expo_router_tabs/src/app/_layout.tsx
+++ 04 - expo_router_tabs/src/app/_layout.tsx
@@ -3,8 +3,12 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen
+      {/* <Stack.Screen
         name='(tabs)'
+        options={{ headerShown: false }}
+      /> */}
+      <Stack.Screen
+        name='(drawer)'
         options={{ headerShown: false }}
       />
     </Stack>
