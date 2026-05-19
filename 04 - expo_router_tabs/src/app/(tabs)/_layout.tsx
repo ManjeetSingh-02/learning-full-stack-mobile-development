f1080bddca71b25e868758d8bf02a27080ea50cb
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-20 00:16:26 +0530

    Refactor tab layout to use NativeTabs and separate JS_Tabs function

--- 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
+++ 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
@@ -4,48 +4,59 @@ import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
-    // <Tabs>
-    //   <Tabs.Screen
-    //     name='index'
-    //     options={{
-    //       title: 'Home',
-    //       tabBarIcon: ({ color }) => (
-    //         <Ionicons
-    //           name='home'
-    //           size={24}
-    //           color={color}
-    //         />
-    //       ),
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name='explore'
-    //     options={{
-    //       title: 'Explore',
-    //       tabBarIcon: ({ color }) => (
-    //         <Ionicons
-    //           name='compass'
-    //           size={24}
-    //           color={color}
-    //         />
-    //       ),
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name='about'
-    //     options={{
-    //       title: 'About',
-    //       tabBarIcon: ({ color }) => (
-    //         <Ionicons
-    //           name='information'
-    //           size={24}
-    //           color={color}
-    //         />
-    //       ),
-    //     }}
-    //   />
-    // </Tabs>
+    // <JS_Tabs />
+    <Native_Tabs />
+  );
+}
 
+function JS_Tabs() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='home'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='explore'
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='compass'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='about'
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='information'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+function Native_Tabs() {
+  return (
     <NativeTabs>
       <NativeTabs.Trigger name='index'>
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
