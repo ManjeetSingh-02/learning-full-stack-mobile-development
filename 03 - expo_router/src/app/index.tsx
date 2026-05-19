1dd4d5139479b4b0529c5a0fcc42221e6b106c22
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-19 22:50:46 +0530

    Add static and dynamic routes with navigation buttons

--- 03 - expo_router/src/app/index.tsx
+++ 03 - expo_router/src/app/index.tsx
@@ -1,9 +1,54 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { Button } from '@react-navigation/elements';
+import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      {/* Static Routes */}
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/about'>Static File About</Link>
+      </Button>
+
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/contact'>Static Folder Contact/Index</Link>
+      </Button>
+
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/contact/locations'>Static Folder Contact/Locations</Link>
+      </Button>
+
+      {/* Dynamic Routes */}
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/123'>Dynamic File UserID(123)</Link>
+      </Button>
+
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/docs/expo/guide'>Dynamic Folder Docs/(Expo)/Guide/Index</Link>
+      </Button>
+
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/docs/expo/guide/1'>Dynamic Folder Docs/(Expo)/Guide/(1)</Link>
+      </Button>
+
+      {/* Catch All Routes */}
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/products/name'>Catch ALL Products/(name)</Link>
+      </Button>
+
+      {/* @ts-ignore */}
+      <Button>
+        <Link href='/products/name/overview/types'>
+          Catch ALL Products/(name)/(overview)/(types)
+        </Link>
+      </Button>
     </View>
   );
 }
