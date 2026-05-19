9c962449ceb1fb98308a31867c8c99204cf63f8a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-20 00:07:21 +0530

    Implement tab navigation with Home, Explore, and About screens

--- 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
+++ 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
@@ -1,5 +1,48 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='home'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='explore'
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='compass'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='about'
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name='information'
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
