752543b07b5068245d4460374a712d2241006c61
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-19 23:06:37 +0530

    Refactor RootLayout to use View and Slot for structured layout with header and footer

--- 03 - expo_router/src/app/_layout.tsx
+++ 03 - expo_router/src/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
+import { Text, View } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 10 }}>
+      <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Header</Text>
+      <Slot />
+      <Text style={{ borderColor: 'black', borderWidth: 2, padding: 4 }}>Footer</Text>
+    </View>
+  );
 }
