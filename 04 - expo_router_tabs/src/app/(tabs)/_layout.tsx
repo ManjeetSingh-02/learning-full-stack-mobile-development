4d6445013a9f15036f22246c87264350e3da0ad0
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-20 00:11:43 +0530

    Replace Tabs with NativeTabs for improved tab navigation implementation

--- 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
+++ 04 - expo_router_tabs/src/app/(tabs)/_layout.tsx
@@ -1,48 +1,75 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name='index'
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name='home'
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='explore'
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name='compass'
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='about'
-        options={{
-          title: 'About',
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name='information'
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+    // <Tabs>
+    //   <Tabs.Screen
+    //     name='index'
+    //     options={{
+    //       title: 'Home',
+    //       tabBarIcon: ({ color }) => (
+    //         <Ionicons
+    //           name='home'
+    //           size={24}
+    //           color={color}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name='explore'
+    //     options={{
+    //       title: 'Explore',
+    //       tabBarIcon: ({ color }) => (
+    //         <Ionicons
+    //           name='compass'
+    //           size={24}
+    //           color={color}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name='about'
+    //     options={{
+    //       title: 'About',
+    //       tabBarIcon: ({ color }) => (
+    //         <Ionicons
+    //           name='information'
+    //           size={24}
+    //           color={color}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    // </Tabs>
+
+    <NativeTabs>
+      <NativeTabs.Trigger name='index'>
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf='house.fill'
+          md='home'
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name='explore'>
+        <NativeTabs.Trigger.Icon
+          sf='safari'
+          md='explore'
+        />
+        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name='about'>
+        <NativeTabs.Trigger.Icon
+          sf='info.circle.fill'
+          md='info'
+        />
+        <NativeTabs.Trigger.Label>About</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
