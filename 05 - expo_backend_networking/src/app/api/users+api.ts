a546e2fc9562ccdd66bf577b4a5d453a208b76ce
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-23 23:05:11 +0530

    Refactor database queries in users API to use template literals

--- 05 - expo_backend_networking/src/app/api/users+api.ts
+++ 05 - expo_backend_networking/src/app/api/users+api.ts
@@ -2,8 +2,22 @@ import { db } from '@/lib/db';
 
 export async function GET(_request: Request) {
   try {
-    const query = await db.query('SELECT * FROM users_data');
+    const query = await db`SELECT * FROM users_data`;
     return Response.json({ data: query }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: error }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const query = await db`
+      INSERT INTO users_data (name, email)
+      VALUES (${body.name}, ${body.email})
+      RETURNING *
+    `;
+    return Response.json({ data: query }, { status: 201 });
   } catch (error) {
     return Response.json({ error: error }, { status: 500 });
   }
