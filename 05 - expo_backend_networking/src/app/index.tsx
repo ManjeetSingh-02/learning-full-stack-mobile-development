4244df2b05c13be4efc66a2ef5fa1557a2a7e9c6
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-05-23 21:21:15 +0530

    Fetch and display data from API in Index component

--- 05 - expo_backend_networking/src/app/index.tsx
+++ 05 - expo_backend_networking/src/app/index.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
+  const [data, setData] = useState<null | { title: string }>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>{data && data.title}</Text>
     </View>
   );
 }
