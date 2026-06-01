cc16fb37c70f19a5b921823b08d2f9722af43329
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-01 22:38:23 +0530

    template added

---
+++ 07 - expo_sensors/eslint.config.js
@@ -0,0 +1,11 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  globalIgnores(['node_modules', '.expo']),
+
+  expoConfig,
+
+  eslintPluginPrettierRecommended,
+]);
