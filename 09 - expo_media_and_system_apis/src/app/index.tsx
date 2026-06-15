6cbfe0f18614ee396365b267f3b70b5d2ce126d3
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-16 00:59:40 +0530

    Add gap between buttons in Index component for improved layout

--- 09 - expo_media_and_system_apis/src/app/index.tsx
+++ 09 - expo_media_and_system_apis/src/app/index.tsx
@@ -5,8 +5,9 @@ export default function Index() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 }}>
       <Button title="Camera Photo" onPress={() => router.push('/camera_photo')} />
+      <Button title="Camera Video" onPress={() => router.push('/camera_video')} />
     </View>
   );
 }
