045a64b5f34208cc3a620c2361d32a14c0333aca
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-21 03:06:07 +0530

    Implement local notification functionality with button trigger

--- 10 - expo_notifications/src/app/index.tsx
+++ 10 - expo_notifications/src/app/index.tsx
@@ -1,9 +1,47 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function Index() {
+  async function localNotification() {
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status !== 'granted') return;
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        data: { screen: '/profile', userID: 123 },
+        sound: 'default',
+        body: 'This is local notification',
+        priority: Notifications.AndroidNotificationPriority.HIGH,
+        title: 'Local Notification',
+        subtitle: 'This is subtitle',
+        sticky: true,
+      },
+      trigger: null,
+    });
+  }
+
+  useEffect(() => {
+    Notifications.addNotificationResponseReceivedListener(response =>
+      console.log(response.notification.request.content.data)
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Notifications</Text>
+      <Button
+        title='Press to send a local notification'
+        onPress={localNotification}
+      />
     </View>
   );
 }
