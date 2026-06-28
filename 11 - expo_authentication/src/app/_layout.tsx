463ace720196c286b447699aa89ce56a3893b6b5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-28 06:21:19 +0530

    Wrap Stack component with AuthProvider for improved authentication context

--- 11 - expo_authentication/src/app/_layout.tsx
+++ 11 - expo_authentication/src/app/_layout.tsx
@@ -1,17 +1,20 @@
+import { AuthProvider } from '@/context/auth.context';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   const isLoggedIn = false;
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={!isLoggedIn}>
-        <Stack.Screen name='(auth)/login' />
-      </Stack.Protected>
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name='(auth)/login' />
+        </Stack.Protected>
 
-      <Stack.Protected guard={isLoggedIn}>
-        <Stack.Screen name='index' />
-      </Stack.Protected>
-    </Stack>
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name='index' />
+        </Stack.Protected>
+      </Stack>
+    </AuthProvider>
   );
 }
