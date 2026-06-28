40e006ad8f5d593d5829d14f059b9fc4eef387d2
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-28 06:32:53 +0530

    Refactor RootLayout to utilize useAuth for user state management and loading indicator

--- 11 - expo_authentication/src/app/_layout.tsx
+++ 11 - expo_authentication/src/app/_layout.tsx
@@ -1,17 +1,20 @@
-import { AuthProvider } from '@/context/auth.context';
+import { AuthProvider, useAuth } from '@/context/auth.context';
 import { Stack } from 'expo-router';
+import { ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
-  const isLoggedIn = false;
+  const { user, isReady } = useAuth();
+
+  if (!isReady) return <ActivityIndicator size='large' />;
 
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Protected guard={user === null}>
           <Stack.Screen name='(auth)/login' />
         </Stack.Protected>
 
-        <Stack.Protected guard={isLoggedIn}>
+        <Stack.Protected guard={user !== null}>
           <Stack.Screen name='index' />
         </Stack.Protected>
       </Stack>
