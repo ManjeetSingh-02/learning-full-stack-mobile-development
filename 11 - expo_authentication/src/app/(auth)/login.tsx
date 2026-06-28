30323e86ede9977140dcde5a9ddd6788450d91e5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-06-28 06:31:26 +0530

    Implement sign-in functionality for Google and GitHub in Login component

--- 11 - expo_authentication/src/app/(auth)/login.tsx
+++ 11 - expo_authentication/src/app/(auth)/login.tsx
@@ -1,10 +1,19 @@
+import { useAuth } from '@/context/auth.context';
 import { View, StyleSheet, Button } from 'react-native';
 
 export default function Login() {
+  const { signIn } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Button title='Login with Google' />
-      <Button title='Login with GitHub' />
+      <Button
+        title='Login with Google'
+        onPress={() => signIn('google')}
+      />
+      <Button
+        title='Login with GitHub'
+        onPress={() => signIn('github')}
+      />
     </View>
   );
 }
